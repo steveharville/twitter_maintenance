dd1c2b72686387788394dfe83c4c0060169abec2
Steve Harville <steve.harville@gmail.com>
2022-04-28 12:44:57 -0400

    Update twitter_maintenance_follow_unfollow.py

--- twitter_maintenance_follow_unfollow.py
+++ twitter_maintenance_follow_unfollow.py
@@ -5,8 +5,6 @@ import twitter_util2
 client_name,target_name=twitter_util2.get_client_target_names()
 db_connection,db_cursor=twitter_util2.setup_database(client_name, target_name)
 twt=twitter_util2.get_twitter_handle()
-#twitter_util2.load_client_tables(twt,db_connection,db_cursor,client_name)
-#twitter_util2.load_target_tables(twt,db_connection,db_cursor,target_name)
 twitter_util2.summarize_current_stats(db_connection,db_cursor)
 more_to_follow = True
 more_to_unfollow = True
